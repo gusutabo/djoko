ef6fe5b3e14fa9dc0897b8968e1fee97afaccd21
gusutabo <gustavolinssm@gmail.com>
2026-04-07 17:56:35 -0300

    chore: update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject djoko "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://example.com/FIXME"
+  :description "A small terminal tennis game simulator written in Clojure."
+  :url "https://github.com/gusutabo/djoko"
   :license {:name "MIT License"
           :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.12.2"]]
+  :main ^:skip-aot djoko.core
   :repl-options {:init-ns djoko.core})
